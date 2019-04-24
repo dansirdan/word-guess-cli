e53c6ab5fcef369472dfc22e913eb5ddfc82e6b3
dansirdan <daniel.monteton@gmail.com>
2019-04-23 23:37:10 -0600

    word guess game FINAL version 1.0

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # word-guess-cli
-command line word guess game
+
+Pokemon Word-Guess-Game 
+version 1.0
+
+NPM Packages Used
+- inquirer
+- pokemon
+- chalk
+
+Using the terminal/command-line while in the location of the directory app, simply type "node" followed by "./index.js" (no quotations please);
+
+Instructions:
+1. Type a 'single' letter into the prompt to attempt a catch of that letter...
+2. You only have 10 pokeballs to try and 'catch' the Pokemon's full name...
+3. A successful catch returns a Pokeball to you...
+4. An unsuccessful catch causes you to lose a Pokeball... 
+5. If you fail to catch the Pokemon's full name, you lose...
+6. Shiny Pokemon exist.
+
+EXTRA FEATURES:
+
++ FANCY Print methods, creating a more pleasurable viewing experience.
+<img src="./1.JPG">
+
++ SHINY Pokemon
+<img src="./2.JPG">
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const Word = require("./word.js");
 const inquirer = require("inquirer");
 const pokemon = require("pokemon");
+const chalk = require("chalk");
+const log = console.log;
 
 const words = pokemon.all();
 var score = 0;
@@ -8,9 +10,27 @@ var shinyScore = 0;
 var guessLeft = 10;
 
 function init() {
+
   score = 0;
-  console.log(`\n--?WHO'S THAT POKEMON?--\n`)
+  shinyScore = 0;
+
+  log(chalk.yellow(`\n\n██████╗  ██████╗ ██╗  ██╗███████╗███╗   ███╗ ██████╗ ███╗   ██╗`));
+  log(chalk.yellow(`██╔══██╗██╔═══██╗██║ ██╔╝██╔════╝████╗ ████║██╔═══██╗████╗  ██║`));
+  log(chalk.yellow(`██████╔╝██║   ██║█████╔╝ █████╗  ██╔████╔██║██║   ██║██╔██╗ ██║`));
+  log(chalk.yellow(`██╔═══╝ ██║   ██║██╔═██╗ ██╔══╝  ██║╚██╔╝██║██║   ██║██║╚██╗██║`));
+  log(chalk.yellow(`██║     ╚██████╔╝██║  ██╗███████╗██║ ╚═╝ ██║╚██████╔╝██║ ╚████║`));
+  log(chalk.yellow(`╚═╝      ╚═════╝ ╚═╝  ╚═╝╚══════╝╚═╝     ╚═╝ ╚═════╝ ╚═╝  ╚═══╝`));
+  log(chalk.yellow(`                     GOTTA CATCH 'EM ALL!                      `));
+  log(chalk.green(`\nINSTRUCTIONS`));
+  log(chalk.green(`1. Type a 'single' letter into the prompt to attempt a catch of that letter...`));
+  log(chalk.green("2. You only have 10 pokeballs to try and 'catch' the Pokemon's full name..."));
+  log(chalk.green("3. A successful catch returns a Pokeball to you..."));
+  log(chalk.green("4. An unsuccessful catch causes you to lose a Pokeball..."));
+  log(chalk.green("5. If you fail to catch the Pokemon's full name, you lose..."));
+  log(chalk.green("6. Shiny Pokemon exist.\n\n"));
+
   newRound()
+
 };
 
 function newRound() {
@@ -32,23 +52,42 @@ function guessLetter(guessWord) {
   if (guessLeft < 1) {
 
     console.log("\n\nYou're out of Pokeballs...\n\n");
-    console.log("\nGAME OVER\n")
+    console.log(`${guessWord.word} ran away!\n\n`)
     continueGame();
 
   } else if (guessLeft > 0) {
 
     inquirer.prompt([{
         name: "guess",
-        message: "\nGuess a Letter: "
+        message: "Guess a Letter: "
       }])
       .then(data => {
+
         let userGuess = data.guess.toUpperCase();
+
+        if (Number.isInteger(data.guess)) {
+          userGuess = data.guess;
+        };
+
         console.log(`\n\n`)
 
         if (userGuess == guessWord.word) {
 
-          console.log("PERFECT CATCH! You guessed the entire Pokemon's name!");
-          console.log(`\n*SHINY*${guessWord.word} joins the team!\n`);
+          console.log("PERFECT CATCH! You guessed the entire Pokemon's name!\n");
+          log(chalk.red(`        ▄███████████▄`));
+          log(chalk.red(`     ▄██▓▓▓▓▓▓▓▓▓▓▓▓▓██▄`));
+          log(chalk.red(`  ▄█▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓█▄`));
+          log(chalk.red(` ██▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓██`));
+          log(chalk.red(`██▓▓▓▓▓▓▓▓▓███████▓▓▓▓▓▓▓▓▓██`));
+          log(chalk.red(`██▓▓▓▓▓▓▓▓██     ██▓▓▓▓▓▓▓▓██`));
+          log(chalk.white(`███████████   █   ███████████`));
+          log(chalk.white(`██░░░░░░░░██     ██░░░░░░░░██`));
+          log(chalk.white(`██░░░░░░░░░███████░░░░░░░░░██`));
+          log(chalk.white(` ██░░░░░░░░░░░░░░░░░░░░░░░██`));
+          log(chalk.white(`   ▀█░░░░░░░░░░░░░░░░░░░█▀`));
+          log(chalk.white(`     ▀██░░░░░░░░░░░░░██▀`));
+          log(chalk.white(`        ▀███████████▀`));
+          console.log(`\n*SHINY*${guessWord.word} joins the team!\n\n`);
           shinyScore++;
           score++;
           console.log(`You have successfully captured a Pokemon!\nTotal Count: ${score}\n\nShiny Count: ${shinyScore}\n`);
@@ -94,17 +133,17 @@ function continueGame() {
 
 function checkGuess(guessWord, userGuess) {
   guessWord.checkIt(userGuess);
-  guessWord.display();
 
   if (!guessWord.wordArr.includes(userGuess)) {
 
     guessLeft--;
-    console.log(`Oh shoot, you were so close!`)
-    console.log(`\nguesses left: ${guessLeft}\n`);
+    console.log(`You threw a Pokeball...\n\n(╯°□°)╯︵◓\n\n`)
+    console.log(`\nPokeballs left: ${guessLeft}\n`);
     guessWord.display();
     guessLetter(guessWord);
 
   } else {
+    guessWord.display();
     guessLetter(guessWord);
   }
   var compareWord = "";
@@ -115,9 +154,23 @@ function checkGuess(guessWord, userGuess) {
   })
 
   if (guessWord.word == compareWord) {
-    console.log(`\n\n${guessWord.word} joins the team!\n\n`);
+    log(`\n${guessWord.word} joins the team!\n`);
+    log(chalk.red(`        ▄███████████▄`));
+    log(chalk.red(`     ▄██▓▓▓▓▓▓▓▓▓▓▓▓▓██▄`));
+    log(chalk.red(`  ▄█▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓█▄`));
+    log(chalk.red(` ██▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓██`));
+    log(chalk.red(`██▓▓▓▓▓▓▓▓▓███████▓▓▓▓▓▓▓▓▓██`));
+    log(chalk.red(`██▓▓▓▓▓▓▓▓██     ██▓▓▓▓▓▓▓▓██`));
+    log(chalk.white(`███████████   █   ███████████`));
+    log(chalk.white(`██░░░░░░░░██     ██░░░░░░░░██`));
+    log(chalk.white(`██░░░░░░░░░███████░░░░░░░░░██`));
+    log(chalk.white(` ██░░░░░░░░░░░░░░░░░░░░░░░██`));
+    log(chalk.white(`   ▀█░░░░░░░░░░░░░░░░░░░█▀`));
+    log(chalk.white(`     ▀██░░░░░░░░░░░░░██▀`));
+    log(chalk.white(`        ▀███████████▀`));
+    log(`\n\n`);
     score++;
-    console.log(`You have successfully captured a Pokemon!\nTotal Count: ${score}\n\nShiny Count: ${shinyScore}`)
+    console.log(`You have successfully captured ${guessWord.word}!\nTotal Count: ${score}\n\nShiny Count: ${shinyScore}\n`)
     guessWord.letterArray = [];
     guessWord.wordArr = [];
     continueGame();
